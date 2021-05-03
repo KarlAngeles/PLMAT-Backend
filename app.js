6d79ab3169ad4cbb9bdceb87573cabd828f3b3c9
KenjiroRE <karl.robert.angeles.01@gmail.com>
2021-05-03 17:28:11 +0800

    added eligible route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mountRoutes = require('./routes/index')
 
 const app = express()
 
+// will add a util here that verifies if request contains
+// access token given from this express server 
+// needs to included with every request to this api
+
 app.use(express.json())
 mountRoutes(app)
 
--- routes/plmat.js
+++ routes/plmat.js
@@ -5,4 +5,9 @@ plmatRouter.post('/login', async (req, res, next) => {
   res.json(200).end()
 })
 
+plmatRouter.get('/eligible', async (req, res, next) => {
+  console.log(req)
+  res.json({ eligible: false }).end()
+})
+
 module.exports = plmatRouter
