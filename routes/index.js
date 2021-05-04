0754776557840daf580b246cac44d2b154616504
KenjiroRE <karl.robert.angeles.01@gmail.com>
2021-05-04 22:28:22 +0800

    authenticated tokens from client

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const adminRouter = require('./admin')
 const plmatRouter = require('./plmat')
 
 module.exports = (app) => {
-  app.use('/api/admin', adminRouter)
   app.use('/api/plmat', plmatRouter)
+  app.use('/api/admin', adminRouter)
 }
